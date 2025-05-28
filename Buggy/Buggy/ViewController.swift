300344a104c6221b5ca055b596672cde3ed46226
Oye Oloyede <oyekunlemac@gmail.com>
2025-05-28 20:01:44 +0100

    print statement debugging

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+    }
 }
 
