a166570c5eabc187165082342bd518f4fb042b81
Oye Oloyede <oyekunlemac@gmail.com>
2025-05-28 22:25:14 +0100

    added some mock bad code for debugging purposes

--- Buggy/Buggy/ViewController.swift
+++ Buggy/Buggy/ViewController.swift
@@ -15,6 +15,20 @@ class ViewController: UIViewController {
 
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Method: \(#function) in file: \(#file) line: \(#line) called.")
+        
+        badMethod()
+    }
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        // go one step too far when emptying
+        for _ in 0...10 {
+            array.removeObject(at: 0)
+        }
     }
 }
-
