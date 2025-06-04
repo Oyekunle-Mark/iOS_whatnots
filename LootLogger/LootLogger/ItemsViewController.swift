d2d90dfd867f7cd0b829433dd0d10d173bc40394
Oye Oloyede <oyekunlemac@gmail.com>
2025-06-04 10:09:24 +0100

    added custom itemcell

--- LootLogger/LootLogger/ItemsViewController.swift
+++ LootLogger/LootLogger/ItemsViewController.swift
@@ -16,15 +16,17 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         // set the text on the cell with the description of the item that is at the nth index of items,
         // where n = row this cell will appear in on the table view
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+
         return cell
     }
     
@@ -74,5 +76,12 @@ class ItemsViewController: UITableViewController {
             // enter editing mode
             setEditing(true, animated: true)
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
     }
 }
