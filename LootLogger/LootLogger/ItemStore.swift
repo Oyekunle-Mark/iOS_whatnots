010c509864bfb03567776414263fea98cb235047
Oye Oloyede <oyekunlemac@gmail.com>
2025-06-12 21:21:29 +0100

    Added saving to file system

--- LootLogger/LootLogger/ItemStore.swift
+++ LootLogger/LootLogger/ItemStore.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    let itemArchiveURL: URL = {
+        let documentsDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = documentsDirectories.first!
+        
+        return documentsDirectory.appendingPathComponent("items.plist")
+    }()
     
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
@@ -32,5 +38,26 @@ class ItemStore {
         let movedItem = allItems[fromIndex]
         allItems.remove(at: fromIndex)
         allItems.insert(movedItem, at: toIndex)
+    }
+    
+    @objc func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL)")
+
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(allItems)
+            try data.write(to: itemArchiveURL, options: [.atomic])
+            print("Saved all the items.")
+            return true
+        } catch let encodingError {
+            print("Error encoding allItems: \(encodingError)")
+        }
+        
+        return false
+    }
+    
+    init() {
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(saveChanges), name: UIScene.didEnterBackgroundNotification, object: nil)
     }
 }
