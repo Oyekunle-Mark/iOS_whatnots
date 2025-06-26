a972759fda9710a02743fec9819d54dc2a2e2c67
Oye Oloyede <oyekunlemac@gmail.com>
2025-06-26 18:18:29 +0100

    BAsic setup of mood seletion and logging

--- Mandala/Mandala/MoodSelectionViewController.swift
+++ Mandala/Mandala/MoodSelectionViewController.swift
@@ -45,12 +45,37 @@ class MoodSelectionViewController: UIViewController {
         }
     }
     
+    var moodsConfigurable: MoodsConfigurable!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "embedContainerViewController":
+            guard let moodsConfigurable = segue.destination as? MoodsConfigurable else {
+                preconditionFailure("View controller expected to conform to MoodsConfigurable")
+            }
+            
+            self.moodsConfigurable = moodsConfigurable
+            segue.destination.additionalSafeAreaInsets = UIEdgeInsets(top: 0, left: 0, bottom: 160, right: 0)
+        default:
+            preconditionFailure("Unexpected segue identifier")
+        }
+    }
+    
     @objc func moodSelectionChanged(_ sender: UIButton) {
         guard let selectedIndex = moodButtons.firstIndex(of: sender) else {
             preconditionFailure("Unable to find the tapped button in the buttons array.")
         }
         
         currentMood = moods[selectedIndex]
+    }
+    
+    @IBAction func addMoodTapped(_ sender: Any) {
+        guard let currentMood = currentMood else {
+            return
+        }
+        
+        let newMoodEntry = MoodEntry(mood: currentMood, timestamp: Date())
+        moodsConfigurable.add(newMoodEntry)
     }
 
     override func viewDidLoad() {
---
+++ Mandala/Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by omarkoloyede on 6/26/25.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
