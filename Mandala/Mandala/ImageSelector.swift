a882cf81922d612e955897a486063e757d65f937
Oye Oloyede <oyekunlemac@gmail.com>
2025-06-28 22:47:52 +0100

    completed adding the uicontrol

--- Mandala/Mandala/ImageSelector.swift
+++ Mandala/Mandala/ImageSelector.swift
@@ -22,16 +22,39 @@ class ImageSelector: UIControl {
     
     private func configureViewHierarchy() {
         addSubview(selectorStackView)
+        insertSubview(highlightView, belowSubview: selectorStackView)
         
         NSLayoutConstraint.activate([
             selectorStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             selectorStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
             selectorStackView.topAnchor.constraint(equalTo: topAnchor),
             selectorStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            highlightView.heightAnchor.constraint(equalTo: highlightView.widthAnchor),
+            highlightView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.9),
+            highlightView.centerYAnchor.constraint(equalTo: selectorStackView.centerYAnchor),
         ])
     }
     
-    var selectedIndex = 0
+    private var highlightViewXConstraint: NSLayoutConstraint! {
+        didSet {
+            oldValue?.isActive = false
+            highlightViewXConstraint.isActive = true
+        }
+    }
+    
+    var selectedIndex = 0 {
+        didSet {
+            if selectedIndex < 0 {
+                selectedIndex = 0
+            }
+            if selectedIndex >= imageButtons.count {
+                selectedIndex = imageButtons.count - 1
+            }
+            
+            let imageButton = imageButtons[selectedIndex]
+            highlightViewXConstraint = highlightView.centerXAnchor.constraint(equalTo: imageButton.centerXAnchor)
+        }
+    }
     
     private var imageButtons: [UIButton] = [] {
         didSet {
@@ -57,6 +80,13 @@ class ImageSelector: UIControl {
         }
     }
     
+    private let highlightView: UIView = {
+        let view = UIView()
+        view.backgroundColor = view.tintColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     @objc private func imageButtonTapped(_ sender: UIButton) {
         guard let buttonIndex = imageButtons.firstIndex(of: sender) else {
             preconditionFailure("The buttons and images are not parallel.")
@@ -74,5 +104,11 @@ class ImageSelector: UIControl {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         configureViewHierarchy()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        highlightView.layer.cornerRadius = highlightView.bounds.width / 2.0
     }
 }
