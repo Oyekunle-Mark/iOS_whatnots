cd12ccf85c4ddeac593525c781dd062429cded71
Oye Oloyede <oyekunlemac@gmail.com>
2025-07-05 23:22:01 +0100

    photorama: now using uicollection to show images with a loader

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by omarkoloyede on 7/5/25.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(displaying image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
